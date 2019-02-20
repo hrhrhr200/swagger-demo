8caa14153398993e3b0b6eda65068e4c4e5cfbce
huangrui <huangrui01@edspay.com>
2019-02-20 15:00:09 +0800

    初始化项目

---
+++ src/main/java/com/example/swagger/swaggerdemo/Swagger2.java
@@ -0,0 +1,24 @@
+package com.example.swagger.swaggerdemo;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger2配置类
+ * 在与spring boot 集成时，放在application同级目录下
+ * 通过Configuration 注解加载配置
+ * 通过EnableSwagger2注解来启用Swagger2
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    /**
+     * 创建API应用
+     * apiInfo() 增加API相关信息
+     * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
+     * 本例采用指定扫描的包路径来定义指定要建立API的目录。
+     *
+     * @return
+     */
+}
